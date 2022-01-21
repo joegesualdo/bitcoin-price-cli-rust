24fa957e5f0886d1919e689f6f25adb5c6a5cdc5
Joe Gesualdo <joe@joes-air.myfiosgateway.com>
2022-01-21 12:12:30 -0500

    implement main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use bitcoin_price;
+
 fn main() {
-    println!("Hello, world!");
+    let price = bitcoin_price::get_coinbase_price();
+    let price_string = price.to_string();
+    println!("{}", price_string);
 }
